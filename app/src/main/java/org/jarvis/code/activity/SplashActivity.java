f9f942f2f4dcc3cebbdbee53cdf8dfab95b9a073
kimchheng <kimchheng101@gmail.com>
2017-09-01 23:16:04 +0700

    update splash activity

--- app/src/main/java/org/jarvis/code/activity/SplashActivity.java
+++ app/src/main/java/org/jarvis/code/activity/SplashActivity.java
@@ -2,19 +2,38 @@ package org.jarvis.code.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+
+import org.jarvis.code.R;
 
 /**
  * Created by KimChheng on 7/16/2017.
  */
 
 public class SplashActivity extends AppCompatActivity {
+    private static boolean splashLoaded = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        if (!splashLoaded) {
+            setContentView(R.layout.activity_splash);
+            int secondsDelayed = 1;
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }, secondsDelayed * 500);
+
+            splashLoaded = true;
+        }
+        else {
+            Intent goToMainActivity = new Intent(SplashActivity.this, MainActivity.class);
+            goToMainActivity.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            startActivity(goToMainActivity);
+            finish();
+        }
     }
 }
